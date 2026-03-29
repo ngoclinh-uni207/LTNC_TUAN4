e1db950f65088c283dea7a2249fbb2f156921bca
ngoclinh-uni207 <25023306@vnu.edu.vn>
2026-03-30 00:07:08 +0700

    Thêm đối tượng Cat3 cho file lambda.java

--- src/lambda/Main.java
+++ src/lambda/Main.java
@@ -9,5 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Animal cat2 = (firstName,lastName) -> "Họ và tên: "+ firstName+ " " + lastName ;
         System.out.println(cat2.speak("Linh","Vo"));
+        Animal cat3 =(firstName,lastName) -> "Họ và tên: "+ firstName+ " " + lastName ;
+        System.out.println(cat3.speak("Meo","Mèo"));
     }
 }
